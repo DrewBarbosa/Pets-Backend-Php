338ef013f8fb2d77a6a72999f4c3c88d799a94c6
Endrew Barbosa <endrew.ab@hotmail.com>
2022-06-06 22:50:32 -0300

    Método de adicionar pet

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pet;
+use App\Http\Requests\PetRequest;
 use Illuminate\Http\Request;
 
 class PetController extends Controller
@@ -15,5 +16,20 @@ class PetController extends Controller
     public function index()
     {
         return Pet::get();
+    }
+
+    /**
+     * Cria um pet no banco de dados
+     *
+     * @param PetRequest $request
+     * @return Pet
+     */
+    public function store(PetRequest $request)
+    {
+
+        $dadosDoPet = $request->all();
+        
+        return Pet::create($dadosDoPet);
+
     }
 }
